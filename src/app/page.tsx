50c953aaf4dea453584f77cf7b908af7c05a015a
kazuki-310 <kazu.0310123@gmail.com>
2025-04-29 17:57:34 +0900

    fix: WithApolloProviderをRootLayoutに追加し、Homeコンポーネントから削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { WithApolloProvider } from '@/providers/with-apollo-provider';
 import { User } from './_components/user';
 
 export default function Home() {
   return (
     <main>
-      <WithApolloProvider>
-        <User />
-      </WithApolloProvider>
+      <User />
     </main>
   );
 }
