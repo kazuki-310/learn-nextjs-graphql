578c45e9b35a595847b3daf52f8c8b5f137663c0
kazuki-310 <kazu.0310123@gmail.com>
2025-05-03 00:49:21 +0900

    fix: コメントアウトされたコードを整理

--- src/app/user-list.tsx
+++ src/app/user-list.tsx
@@ -1,17 +1,17 @@
-import { getProducts } from '@/lib/graphql/product/queries/get-products';
+// import { getProducts } from '@/lib/graphql/product/queries/get-products';
 
 export async function UserList() {
-  const product = await getProducts();
+  // const product = await getProducts();
 
   return (
     <div className="p-6">
       <ul>
-        {product.products.map((product) => (
+        {/* {product.products.map((product) => (
           <li key={product.id}>
             <h2>{product.title}</h2>
             <p>{product.description}</p>
           </li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
