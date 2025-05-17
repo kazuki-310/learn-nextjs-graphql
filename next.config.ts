36d8b6525d802cd8c6e3844ffb7e65fb879cee2e
kazuki-310 <kazu.0310123@gmail.com>
2025-05-17 18:59:44 +0900

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+    dynamicIO: true,
+    ppr: true,
+  },
+};
 
 export default nextConfig;
