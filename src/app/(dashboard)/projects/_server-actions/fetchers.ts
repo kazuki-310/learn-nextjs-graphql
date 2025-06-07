7d39ec1cab577b026e60c573d6af6f34f43af9df
kazuki-310 <kazu.0310123@gmail.com>
2025-06-07 11:41:19 +0900

    feat: getProjects と getUsers 関数にエラーハンドリングを追加

--- src/app/(dashboard)/projects/_server-actions/fetchers.ts
+++ src/app/(dashboard)/projects/_server-actions/fetchers.ts
@@ -2,12 +2,17 @@ import { graphQLFetchSdk } from '@/lib/graphql';
 import { cache } from 'react';
 
 export const getProjects = cache(async () => {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  try {
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const res = await graphQLFetchSdk.getProjects(undefined, {
-    revalidate: false,
-    tags: ['projects'],
-  });
+    const res = await graphQLFetchSdk.getProjects(undefined, {
+      revalidate: false,
+      tags: ['projects'],
+    });
 
-  return res.projects;
+    return res.projects;
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+    throw error;
+  }
 });
--- src/app/(dashboard)/users/_server-actions/fetchers.ts
+++ src/app/(dashboard)/users/_server-actions/fetchers.ts
@@ -2,10 +2,15 @@ import { graphQLFetchSdk } from '@/lib/graphql';
 import { cache } from 'react';
 
 export const getUsers = cache(async () => {
-  const res = await graphQLFetchSdk.getUsers(undefined, {
-    revalidate: false,
-    tags: ['users'],
-  });
+  try {
+    const res = await graphQLFetchSdk.getUsers(undefined, {
+      revalidate: false,
+      tags: ['users'],
+    });
 
-  return res.users;
+    return res.users;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
 });
